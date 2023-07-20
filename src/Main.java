d4f5d4455b3d8ee30a86fdfb751447b15c49332c
trandux <trandux203@hotmail.com>
2023-07-20 16:37:48 +0200

    Cleaning of main class

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-
-       RoadJunction güngören = new RoadJunction();
-       güngören.whichColor();
-       güngören.switchLamps();
-       güngören.whichColor();
-       güngören.switchLamps();
-       güngören.whichColor();
-       güngören.switchLamps();
-       güngören.whichColor();
-        güngören.switchLamps();
-        güngören.whichColor();
-        güngören.switchLamps();
-        güngören.whichColor();
-        System.out.println(güngören.isCrossingSafe(3));
     }
 }
 
